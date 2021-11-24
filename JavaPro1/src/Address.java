d1cbed2316e26406eb2b7f6a6b9f07a2e8f4a07b
ssanjay8 <ssanjay8@host.docker.internal>
2021-11-24 10:19:24 +0530

    Address Updated

---
+++ JavaPro1/src/Address.java
@@ -0,0 +1,26 @@
+
+public class Address {
+	private int hno;
+	private String colony;
+	private String city;
+	public int getHno() {
+		return hno;
+	}
+	public void setHno(int hno) {
+		this.hno = hno;
+	}
+	public String getColony() {
+		return colony;
+	}
+	public void setColony(String colony) {
+		this.colony = colony;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+
+}
